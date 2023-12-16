404c09645b81daff8dd0bc010e0560461021dee4
felipeverones <felipe_verones@hotmail.com>
2023-12-16 17:06:20 -0300

    first commit

---
+++ pkmnRRhatcheggs.py
@@ -0,0 +1,20 @@
+import pyautogui
+import pydirectinput
+import time
+
+# Configuração de teclas (ajuste conforme a configuração do seu emulador)
+UP = 'up'
+DOWN = 'down'
+LEFT = 'left'
+RIGHT = 'right'
+
+# Exemplo de padrão de movimento (ajuste conforme necessário)
+while True:
+    pydirectinput.keyDown('left')
+    time.sleep(1.5)
+    pydirectinput.keyUp('left')
+    
+    pydirectinput.keyDown('right')
+    time.sleep(1.5)
+    pydirectinput.keyUp('right')
+    
